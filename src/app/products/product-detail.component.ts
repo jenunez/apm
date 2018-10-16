b1c2897dbafd4e089576912d9c498776e555edb0
Jesus Nunez <jesus.nunez@incontact.com>
2018-10-16 10:17:20 -0400

    route with code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 import { IProduct } from './products';
 
@@ -11,7 +11,7 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
@@ -26,6 +26,10 @@ export class ProductDetailComponent implements OnInit {
       'starRating': 3.2,
       'imageUrl': 'https://openclippart.org/image/300px/svg_to_png/26215/anonymous_Leaf_Rake.png'
     }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
